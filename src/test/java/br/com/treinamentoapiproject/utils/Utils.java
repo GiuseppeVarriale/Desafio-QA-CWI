c05f557b6db5c519c8681264b00213d44f86121c
Giuseppe Varriale <pepe.saoleo@gmail.com>
2021-03-25 17:03:16 -0300

    fix : fix base path used to find contracts file

--- src/test/java/br/com/treinamentoapiproject/utils/Utils.java
+++ src/test/java/br/com/treinamentoapiproject/utils/Utils.java
@@ -5,7 +5,7 @@ public class Utils {
 
     public static  String getContractsBasePath(String pack, String contract) {
         return System.getProperty("user.dir")
-                + "/src/test/java/br/com/restassuredapitesting/tests/"
+                + "/src/test/java/br/com/treinamentoapiproject/tests/"
                 + pack
                 + "/contracts/"
                 + contract
