75670aa070f1ea918e88558b58d7c58fae39b087
Giuseppe Varriale <pepe.saoleo@gmail.com>
2021-03-25 15:32:36 -0300

    config - configure RestAssured base URL

--- src/test/java/br/com/treinamentoapiproject/tests/base/tests/BaseTest.java
+++ src/test/java/br/com/treinamentoapiproject/tests/base/tests/BaseTest.java
@@ -1,4 +1,13 @@
 package br.com.treinamentoapiproject.tests.base.tests;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 public class BaseTest {
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI= "https://treinamento-api.herokuapp.com/";
+    }
 }
