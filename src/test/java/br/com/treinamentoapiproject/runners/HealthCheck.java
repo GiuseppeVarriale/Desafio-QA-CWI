19be488d24ab2a90fb9db08acda2035695316a39
Giuseppe Varriale <pepe.saoleo@gmail.com>
2021-03-28 14:29:44 -0300

    test : Create HealthCheck Suit files

---
+++ src/test/java/br/com/treinamentoapiproject/runners/HealthCheck.java
@@ -0,0 +1,22 @@
+package br.com.treinamentoapiproject.runners;
+
+import br.com.treinamentoapiproject.tests.auth.tests.PostAuthTest;
+import br.com.treinamentoapiproject.tests.booking.tests.DeleteBookingTest;
+import br.com.treinamentoapiproject.tests.booking.tests.GetBookingTest;
+import br.com.treinamentoapiproject.tests.booking.tests.PutBookingTest;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.treinamentoapiproject.suites.HealthCheck.class)
+@Suite.SuiteClasses({
+        GetBookingTest.class,
+        PutBookingTest.class,
+        PostAuthTest.class,
+        DeleteBookingTest.class,
+        PostAuthTest.class,
+})
+public class HealthCheck {
+
+}
