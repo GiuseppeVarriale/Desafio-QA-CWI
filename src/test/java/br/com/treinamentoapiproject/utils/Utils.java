44b93222a7b48e6284e2deb3cdf699ec0f499492
Giuseppe Varriale <pepe.saoleo@gmail.com>
2021-03-29 19:38:38 -0300

    feature : Add function to compare Dates and return a bollean value

--- src/test/java/br/com/treinamentoapiproject/utils/Utils.java
+++ src/test/java/br/com/treinamentoapiproject/utils/Utils.java
@@ -2,10 +2,14 @@ package br.com.treinamentoapiproject.utils;
 
 import org.json.simple.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Utils {
 
 
-    public static  String getContractsBasePath(String pack, String contract) {
+    public static String getContractsBasePath(String pack, String contract) {
         return System.getProperty("user.dir")
                 + "/src/test/java/br/com/treinamentoapiproject/tests/"
                 + pack
@@ -14,20 +18,37 @@ public class Utils {
                 + ".json";
     }
 
-    public static JSONObject validPayloadBooking(){
+    public static JSONObject validPayloadBooking() {
         JSONObject payload = new JSONObject();
         JSONObject bookingDates = new JSONObject();
 
-        bookingDates.put("checkin", "2021-01-01");
-        bookingDates.put("checkout","2021-03-06");
+        bookingDates.put("checkin", "2021-03-09");
+        bookingDates.put("checkout", "2021-03-11");
 
-        payload.put("firstname", "Ronaldo");
-        payload.put("lastname", "Fenomeno");
+        payload.put("firstname", "Bob");
+        payload.put("lastname", "Esponja");
         payload.put("totalprice", 123);
         payload.put("depositpaid", true);
         payload.put("bookingdates", bookingDates);
         payload.put("additionalneeds", "Breakfast");
 
         return payload;
+    }
+
+    public static Boolean dateIsBeforeOrEqualThan(String toCompare, String control) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateToCompare = format.parse(toCompare);
+        Date dateControl = format.parse(control);
+
+        return dateToCompare.before(dateControl) || dateToCompare.equals(control);
+    }
+
+
+    public static Boolean dateIsAfterOrEqualThan(String toCompare, String control) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateToCompare = format.parse(toCompare);
+        Date dateControl = format.parse(control);
+
+        return dateToCompare.after(dateControl) || dateToCompare.equals(control);
     }
 }
