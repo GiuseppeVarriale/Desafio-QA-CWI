11b71571faa677199abd8501a1195c60a3d8ee22
Giuseppe Varriale <pepe.saoleo@gmail.com>
2021-03-30 12:09:12 -0300

    test : Create E2e Suite

---
+++ src/test/java/br/com/treinamentoapiproject/runners/E2e.java
@@ -0,0 +1,25 @@
+package br.com.treinamentoapiproject.runners;
+
+
+import br.com.treinamentoapiproject.tests.auth.tests.PostAuthTest;
+import br.com.treinamentoapiproject.tests.booking.requests.PostBookingRequest;
+import br.com.treinamentoapiproject.tests.booking.tests.DeleteBookingTest;
+import br.com.treinamentoapiproject.tests.booking.tests.GetBookingTest;
+import br.com.treinamentoapiproject.tests.booking.tests.PostBookingTest;
+import br.com.treinamentoapiproject.tests.booking.tests.PutBookingTest;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.treinamentoapiproject.suites.E2e.class)
+@Suite.SuiteClasses({
+        DeleteBookingTest.class,
+        GetBookingTest.class,
+        PostBookingTest.class,
+        PutBookingTest.class,
+
+})
+
+public class E2e {
+}
