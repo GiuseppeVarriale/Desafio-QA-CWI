d9d3f031592b7d5fe36c134619855bb190f4a5ee
Giuseppe Varriale <pepe.saoleo@gmail.com>
2021-03-29 21:46:32 -0300

    utils : add java faker to generate payload values

--- src/test/java/br/com/treinamentoapiproject/utils/Utils.java
+++ src/test/java/br/com/treinamentoapiproject/utils/Utils.java
@@ -1,10 +1,13 @@
 package br.com.treinamentoapiproject.utils;
 
+import com.github.javafaker.Faker;
+import org.apache.commons.lang3.time.DateUtils;
 import org.json.simple.JSONObject;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class Utils {
 
@@ -18,19 +21,28 @@ public class Utils {
                 + ".json";
     }
 
-    public static JSONObject validPayloadBooking() {
+    public static JSONObject validPayloadBooking() throws Exception {
         JSONObject payload = new JSONObject();
         JSONObject bookingDates = new JSONObject();
 
-        bookingDates.put("checkin", "2021-03-09");
-        bookingDates.put("checkout", "2021-03-11");
+        Faker faker = new Faker(new Locale("pt-BR"));
+        SimpleDateFormat  format = new SimpleDateFormat("yyyy-MM-dd");
+        Date fakeDateFrom = DateUtils.addYears(new Date(),-5);
+        Date fakeDateTo = DateUtils.addDays(new Date(), 161);
+        Date checkin = faker.date().between(fakeDateFrom,fakeDateTo);
+        fakeDateTo = DateUtils.addDays(checkin, 376);
+        Date checkout = faker.date().between(checkin,fakeDateTo);
 
-        payload.put("firstname", "Bob");
-        payload.put("lastname", "Esponja");
-        payload.put("totalprice", 123);
-        payload.put("depositpaid", true);
+
+        bookingDates.put("checkin", format.format(checkin));
+        bookingDates.put("checkout", format.format(checkout));
+
+        payload.put("firstname", faker.name().firstName());
+        payload.put("lastname", faker.name().lastName());
+        payload.put("totalprice", faker.number().numberBetween(200,4000));
+        payload.put("depositpaid", faker.bool().bool());
         payload.put("bookingdates", bookingDates);
-        payload.put("additionalneeds", "Breakfast");
+        payload.put("additionalneeds", faker.food().dish() + " to lunch");
 
         return payload;
     }
